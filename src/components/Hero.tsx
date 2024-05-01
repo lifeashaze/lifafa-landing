e6e324abefea0887aebe4ccad81bb61848ad3745
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-05-01 17:23:08 +0530

    hero v 0.0.1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,19 @@
+import Image from "next/image";
 import { LifafaHeroMarquee } from "./Marquee";
 import { LifafaHeroMarquee2 } from "./Marquee2";
 
 export function Hero() {
   return (
     <>
-      <div className="text-center h-[75vh]">
-        <h1 className="text-4xl font-bold text-center ">Hero Section</h1>
+      <div className="flex flex-col items-center text-7xl font-bold h-[100vh] leading-normal ">
+        <h1>
+          An <span className="text-primary underline">envelope</span>
+        </h1>
+        <div className="flex items-center gap-2">
+          <Image src="/idk.svg" alt="idk" width={300} height={300} />
+          <h1>of</h1>
+        </div>
+        <h1><span className="font-normal text-secondary">creative</span> solutions</h1>
       </div>
       <LifafaHeroMarquee />
       <LifafaHeroMarquee2 />
